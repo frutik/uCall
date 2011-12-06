6819ab711e254c0c29e4a386bd8bf6a08edb0e7c
Andrew Kornilov <frutik@gmail.com>
2011-12-06 19:21:23 +0200

    -

---
+++ src/web/ucall_backend/local-settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': '@db.backends@',
+        'NAME': '@db.name@',
+        'USER': '@db.user@',
+        'PASSWORD': '@db.password@',
+        'HOST': '@db.host@',
+        'PORT': '@db.port@',
+    }
+}
