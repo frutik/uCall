28853eec1d24e14cc9ee0fa672c0e34ba2196807
Andrew Kornilov <frutik@gmail.com>
2012-01-30 21:04:58 +0200

    added required app

--- web/ucall_backend/settings.py
+++ web/ucall_backend/settings.py
@@ -118,6 +118,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.comments',
+    'django.contrib.sites',
     'config',
     'ucall_ui',
     'ucall',
