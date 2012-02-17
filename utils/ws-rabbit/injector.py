38ef449a4edfac5492bcb1c898ec88bf661db7b8
Andrew Kornilov <frutik@gmail.com>
2012-02-17 20:55:40 +0200

    =

---
+++ utils/ws-rabbit/injector.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import pika
+import sys
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='127.0.0.1'))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='ucall222',
+                         type='direct')
+
+agent = sys.argv[1] if len(sys.argv) > 1 else '14'
+message = ' '.join(sys.argv[2:]) or 'Hello World!'
+
+channel.basic_publish(exchange='ucall222',
+                      routing_key=agent,
+                      body=message)
+print " [x] Sent %r:%r" % (agent, message)
+connection.close()
---
+++ utils/ws-rabbit/tornado-run.py
@@ -0,0 +1,68 @@
+import tornado.ioloop
+import tornado.web
+import tornado.websocket as websocket
+
+from tornado.options import define, options
+
+import os.path
+import uuid
+import time
+
+http_settings = dict(
+    cookie_secret=str(uuid.uuid1()),
+    xsrf_cookies=False,
+    autoescape="xhtml_escape",
+)
+
+define("port", default=8888, help="run on the given port", type=int)
+
+import logging
+logging.basicConfig(level=logging.INFO)
+
+import pika
+from pika_client import PikaClient
+from agent_channel_ws import AgentChannelWebSocket
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r'/agent/([0-9]+)', AgentChannelWebSocket),
+        ]
+        tornado.web.Application.__init__(self, handlers, http_settings)
+
+def main():
+    tornado.options.parse_command_line()
+    app = Application()
+    app.listen(options.port)
+
+	    <replacefilter token="guest" property="pika.username" />
+	    <replacefilter token="guest" property="pika.password" />
+	    <replacefilter token="127.0.0.1" property="pika.host" />
+	    <replacefilter token="5672" property="pika.port" />
+	    <replacefilter token="/" property="pika.virtual_host" />
+	    <replacefilter token="ucall222" property="pika.exchange_name" />
+
+
+    pc = PikaClient(
+        username = 'guest',
+        password = 'guest',
+        host = '127.0.0.1',
+        port = '5672',
+        virtual_host = '/',
+        exchange_name = 'ucall222'
+    )
+    pika.log.setup(color=True)
+
+    app.pika = pc
+
+    ioloop = tornado.ioloop.IOLoop.instance()
+
+    # Add our Pika connect to the IOLoop with a deadline in 0.1 seconds
+    ioloop.add_timeout(time.time() + .1, app.pika.connect)
+
+    # Start the IOLoop
+    ioloop.start()
+
+if __name__ == "__main__":
+    main()
+
